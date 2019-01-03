bb9ac413fda998411b9ecd7a92fed5d9b1d30059
Ari0001 <ariela.haley65@gmail.com>
2019-01-02 17:17:08 -0800

    submit

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -5,12 +5,15 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
-
+    //3 actors - 1. contract testing 2. buyer 3. the seller 4. rando
+    //every contract has an address use to simulate actor
+    //clean room - after each test create new instance 
+    
     // Test for failing conditions in this contracts
     // test that every modifier is working
 
     // buyItem
-
+  
     // test for failure if user does not send enough funds
     // test for purchasing an item that is not for Sale
 
